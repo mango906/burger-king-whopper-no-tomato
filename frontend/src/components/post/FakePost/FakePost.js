668ef647b3f7d466ea7c8f65b50fc1cf9de9197f
Magical Super Granzort <30362922+seojeenyeok@users.noreply.github.com>
2019-05-18 01:47:39 +0900

    [ADD] FakePost
    로딩 상태일 때 보여줄 스켈레톤 컴포넌트

---
+++ frontend/src/components/post/FakePost/FakePost.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./FakePost.scss";
+
+const FakePost = () => {
+    return (
+        <tr className="fake-tr">
+            <td><span className="gray-box" /></td>
+            <td><span className="gray-box" /></td>
+            <td><span className="gray-box" /></td>
+            <td><span className="gray-box" /></td>
+        </tr>
+    );
+};
+
+export default FakePost;
