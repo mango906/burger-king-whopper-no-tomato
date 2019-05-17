79a5025cafc97aec71447c5038a10f30ce843993
Magical Super Granzort <30362922+seojeenyeok@users.noreply.github.com>
2019-05-18 00:25:12 +0900

    [ADD] post model
    게시글 데이터 베이스 모델 작성

---
+++ backend/src/database/models/post.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const crypto = require('crypto');
+const {
+    secret,
+} = require('config/serverconfig.json');
+
+const Comment = Schema({
+    id: { type: Number, require: true, unique: true },
+    author: { type: String, require: true, unique: false },
+    content: { type: String, require: true, unique: false },
+    password: { type: String, require: true, unique: false },
+    created: { type: Date, require: true, unique: false, default: Date.now },
+})
+
+const Post = Schema({
+    id: { type: Number, require: true, unique: true },
+    author: { type: String, require: true, unique: false },
+    title: { type: String, require: true, unique: false },
+    content: { type: String, require: true, unique: false },
+    created: { type: Date, require: true, unique: false, default: Date.now },
+    password: { type: String, require: true, unique: false },
+    comments: { type: [Comment], require: false, unique: false },
+}, {
+        collection: 'post',
+    });
+
+
+
+Post.statics.create = function (id, author, title, content, password) {
+    const resultPassword = crypto.createHmac('sha1', secret)
+        .update(password)
+        .digest('base64');
+
+    return new this({
+        id,
+        author,
+        title,
+        content,
+        password: resultPassword,
+    }).save();
+};
+
+Post.methods.checkPassword = function (password) {
+    const resultPassword = crypto.createHmac('sha1', secret)
+        .update(password)
+        .digest('base64');
+    return this.password === resultPassword;
+};
+
+Comment.statics.create = function (id, author, content, password) {
+    const resultPassword = crypto.createHmac('sha1', secret)
+        .update(password)
+        .digest('base64');
+
+    return new this({
+        id,
+        author,
+        content,
+        password: resultPassword,
+    }).save();
+};
+
+Comment.methods.checkPassword = function (password) {
+    const resultPassword = crypto.createHmac('sha1', secret)
+        .update(password)
+        .digest('base64');
+    return this.password === resultPassword;
+};
+
+module.exports = mongoose.model('Post', Post);
