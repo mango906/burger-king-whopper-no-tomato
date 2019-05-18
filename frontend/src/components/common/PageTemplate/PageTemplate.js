1b6c6a83d49d009a18c25d4e1a9d9a8e0d988620
Magical Super Granzort <30362922+seojeenyeok@users.noreply.github.com>
2019-05-18 15:05:47 +0900

    [FIX] PageTemplate
    로고 클릭시 메인으로 이동,
    작성 화면일시 글쓰기 버튼이 있는 footer 보여주지 않음

--- frontend/src/components/common/PageTemplate/PageTemplate.js
+++ frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import styles from './PageTemplate.scss';
 import classNames from 'classnames/bind';
+import { Link, withRouter } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
-const PageTemplate = ({ children }) => {
+const PageTemplate = ({ children, location }) => {
     return (
         <div className={cx('post-template')}>
             <header>
                 <h1 className={cx('header-title light')}>
-                    버거킹 와퍼에
+                    <Link to="/">
+                        버거킹 와퍼에
                     <br />
-                    토마토 빼고 주세요
+                        토마토 빼고 주세요
+                    </Link>
                 </h1>
             </header>
             {children}
+            {
+                location.pathname !== "/write" &&
+                <footer>
+                    <Link to="/write" className={cx('btn new-post')}>글 쓰기</Link>
+                </footer>
+            }
         </div>
     );
 };
 
-export default PageTemplate;
+export default withRouter(PageTemplate);
