d2b38ca4a828843cdae1d2e18babbcbf4939abbb
Magical Super Granzort <30362922+seojeenyeok@users.noreply.github.com>
2019-05-18 15:04:47 +0900

    [FIX] PostTable
    유동 너비 박스 추가
    오늘 작성한 글일땐 시간이
    아닐땐 날짜가 표시되게 수정

--- frontend/src/components/post/PostTable/PostTable.js
+++ frontend/src/components/post/PostTable/PostTable.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './PostTable.scss';
 import classNames from 'classnames/bind';
 import FakePost from '../FakePost';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -17,11 +18,18 @@ const PostTable = ({ loading, posts }) => {
     }
     if (!posts) return null;
     const postList = posts.map(post => (
-        <tr>
+        <tr key={post.id}>
             <td className={cx('post-id')}>{post.id}</td>
-            <td>{post.author}</td>
-            <td>{post.title}</td>
-            <td>{post.created}</td>
+            <td className={cx('post-author')}>{post.author}</td>
+            <td className={cx('post-content')}>
+                <Link to={`/post/${post.id}`}>{post.title}</Link>
+            </td>
+            <td>{
+                new Date().toLocaleDateString() === new Date(post.created).toLocaleDateString() ?
+                    new Date(post.created).toLocaleTimeString()
+                    :
+                    new Date(post.created).toLocaleDateString()
+            }</td>
         </tr>
     ));
     return (
