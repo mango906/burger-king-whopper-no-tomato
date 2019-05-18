0539a6835949fc02879ee6e1e063b62e8b4fbff1
Magical Super Granzort <30362922+seojeenyeok@users.noreply.github.com>
2019-05-18 22:26:16 +0900

    [FIX] WritePostForm
    자동 완성 안되게 수정

--- frontend/src/components/post/WritePostForm/WritePostForm.js
+++ frontend/src/components/post/WritePostForm/WritePostForm.js
@@ -5,10 +5,10 @@ const WritePostForm = ({ data, onChange, onCancel, onSend }) => {
     return (
         <div className="PostForm">
             <div className="form-inputs">
-                <input value={data.author} onChange={onChange} name="author" className="Input" placeholder="닉네임" />
-                <input value={data.password} onChange={onChange} name="password" className="Input" type="password" placeholder="비밀 번호" />
+                <input value={data.author} onChange={onChange} name="author" className="Input" placeholder="닉네임" autoComplete="off" />
+                <input value={data.password} onChange={onChange} name="password" className="Input" type="password" placeholder="비밀 번호" autoComplete="off" />
                 <br />
-                <input value={data.title} onChange={onChange} name="title" className="Input title" placeholder="제목" />
+                <input value={data.title} onChange={onChange} name="title" className="Input title" placeholder="제목" autoComplete="off" />
                 <textarea value={data.content} onChange={onChange} name="content" className="Input content" placeholder="여기에 내용 입력" />
             </div>
             <div className="form-buttons">
