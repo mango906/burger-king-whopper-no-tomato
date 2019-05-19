56cf7726a194e6d39e38545f4dae289853ea0ee4
Magical Super Granzort <30362922+seojeenyeok@users.noreply.github.com>
2019-05-19 14:14:05 +0900

    [FIX] WritePostForm
    작성창의 최대 길이 설정

--- frontend/src/components/post/WritePostForm/WritePostForm.js
+++ frontend/src/components/post/WritePostForm/WritePostForm.js
@@ -5,10 +5,10 @@ const WritePostForm = ({ data, onChange, onCancel, onSend }) => {
     return (
         <div className="PostForm">
             <div className="form-inputs">
-                <input value={data.author} onChange={onChange} name="author" className="Input" placeholder="닉네임" autoComplete="off" />
-                <input value={data.password} onChange={onChange} name="password" className="Input" type="password" placeholder="비밀 번호" autoComplete="off" />
+                <input value={data.author} onChange={onChange} name="author" className="Input" placeholder="닉네임" autoComplete="off" maxLength="50" />
+                <input value={data.password} onChange={onChange} name="password" className="Input" type="password" placeholder="비밀 번호" autoComplete="off" maxLength="50" />
                 <br />
-                <input value={data.title} onChange={onChange} name="title" className="Input title" placeholder="제목" autoComplete="off" />
+                <input value={data.title} onChange={onChange} name="title" className="Input title" placeholder="제목" autoComplete="off" maxLength="150" />
                 <textarea value={data.content} onChange={onChange} name="content" className="Input content" placeholder="여기에 내용 입력" />
             </div>
             <div className="form-buttons">
