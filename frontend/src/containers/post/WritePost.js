38968e81ef7155ceaab08f8713c821243f132bfc
Magical Super Granzort <30362922+seojeenyeok@users.noreply.github.com>
2019-05-19 14:11:27 +0900

    [FIX] WritePost
    작성 요청 중일때 로더를 화면에 띄움

--- frontend/src/containers/post/WritePost.js
+++ frontend/src/containers/post/WritePost.js
@@ -4,46 +4,61 @@ import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 import { SERVER } from 'config/config.json';
 import { inject } from 'mobx-react';
+import FullscreenLoader from 'components/common/FullScreenLoader';
 
 @inject('store')
 class WritePost extends Component {
-    state = {
-        author: "",
-        title: "",
-        password: "",
-        content: ""
+  state = {
+    loading: false,
+    author: "",
+    title: "",
+    password: "",
+    content: ""
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleCancel = () => {
+    const cancel = window.confirm("글 작성을 취소하시겠습니까?");
+    cancel && this.props.history.goBack();
+  }
+  handleSend = async () => {
+    const { author, title, password, content } = this.state;
+    if (!author || !title || !password || !content) {
+      alert("내용을 모두 작성해주세요");
+      return;
     }
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-    handleCancel = () => {
-        const cancel = window.confirm("글 작성을 취소하시겠습니까?");
-        cancel && this.props.history.goBack();
-    }
-    handleSend = async () => {
-        const { author, title, password, content } = this.state;
-        if (!author || !title || !password || !content) {
-            alert("내용을 모두 작성해주세요");
-            return;
+    this.setState({
+      loading: true,
+    })
+    await axios.post(`${SERVER}/post`, this.state)
+      .then(async res => {
+        const { post } = this.props.store;
+        if (post.apiCall) {
+          post.addPost(res.data);
         }
-        await axios.post(`${SERVER}/post`, this.state)
-            .then(res => {
-                console.log(res);
-                alert("작성에 성공하였습니다.");
-                this.props.store.post.addPost(res.data);
-                this.props.history.push("/");
-            })
-    }
-    componentDidMount() {
-        window.scrollTo(0, 80);
-    }
-    render() {
-        return (
-            <WritePostForm data={this.state} onChange={this.handleChange} onCancel={this.handleCancel} onSend={this.handleSend} />
-        );
-    }
+        this.setState({
+          loading: false,
+        })
+        sessionStorage.setItem("main-scroll", 0);
+        alert("작성에 성공하였습니다.");
+        this.props.history.push("/");
+      })
+  }
+  componentDidMount() {
+    window.scrollTo(0, 80);
+  }
+  render() {
+    const { loading } = this.state;
+    return (
+      <>
+        <WritePostForm data={this.state} onChange={this.handleChange} onCancel={this.handleCancel} onSend={this.handleSend} />
+        {loading && <FullscreenLoader />}
+      </>
+    );
+  }
 }
 
 export default withRouter(WritePost);
