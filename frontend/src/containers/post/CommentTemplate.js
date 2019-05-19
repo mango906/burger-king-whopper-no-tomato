3a09bb0f8136affed6e68aad243de68e90f91ec5
Magical Super Granzort <30362922+seojeenyeok@users.noreply.github.com>
2019-05-19 14:14:34 +0900

    [FIX] CommentTemplate
    댓글 작성 요청 중일때 화면에 로더를 띄움

--- frontend/src/containers/post/CommentTemplate.js
+++ frontend/src/containers/post/CommentTemplate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CommentSection from 'components/comment/CommentSection';
 import CommentList from 'components/comment/CommentList';
+import FullScreenLoader from 'components/common/FullScreenLoader';
 import { inject, observer } from 'mobx-react';
 import axios from 'axios';
 import { SERVER } from 'config/config.json';
@@ -8,38 +9,54 @@ import { SERVER } from 'config/config.json';
 @inject('store')
 @observer
 class CommentTemplate extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: false,
-            author: "",
-            password: "",
-            content: ""
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      author: "",
+      password: "",
+      content: ""
     }
-    handleChange = (e) => {
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+  handleSend = async () => {
+    const { postId } = this.props;
+    const { loading, comments, ...data } = this.state;
+    this.setState({
+      loading: true
+    })
+    await axios.post(`${SERVER}/comment/${postId}`, data)
+      .then(res => {
+        const { comment } = res.data;
+        this.props.store.post.updateViewComments(comment, postId);
         this.setState({
-            [e.target.name]: e.target.value,
+          loading: false,
+          author: "",
+          password: "",
+          content: ""
         })
-    }
-    handleSend = async () => {
-        const { postId } = this.props;
-        const { loading, comments, ...data } = this.state;
-        await axios.post(`${SERVER}/comment/${postId}`, data)
-            .then(res => {
-                const { comment } = res.data;
-                this.props.store.post.updateViewComments(comment);
-            })
-    }
-    render() {
-        const { loading, ...data } = this.state;
-        const { viewComments } = this.props.store.post;
-        return (
-            <CommentSection data={data} onChange={this.handleChange} onSend={this.handleSend}>
-                <CommentList comments={viewComments} />
-            </CommentSection>
-        );
-    }
+      })
+      .catch(e => {
+        alert("댓글 작성에 실패하였습니다");
+        this.setState({
+          loading: false
+        })
+      })
+  }
+  render() {
+    const { loading, ...data } = this.state;
+    const { viewComments } = this.props.store.post;
+    return (
+      <CommentSection data={data} onChange={this.handleChange} onSend={this.handleSend}>
+        <CommentList comments={viewComments} />
+        {loading && <FullScreenLoader />}
+      </CommentSection>
+    );
+  }
 }
 
 export default CommentTemplate;
