13838083e91087ffeb0f98c8a84b8b19987d48f0
Magical Super Granzort <30362922+seojeenyeok@users.noreply.github.com>
2019-05-19 14:20:43 +0900

    [FIX] PostSection
    페이지 이동시 화면 스크롤을 글 목록과 페이지 이동 버튼이 모두 보이도록 설정

--- frontend/src/components/post/PostSection/PostSection.js
+++ frontend/src/components/post/PostSection/PostSection.js
@@ -6,34 +6,44 @@ import createArray from 'lib/createArray';
 const cx = classNames.bind(styles);
 
 const PostSection = ({ children, postCount, onPageMove }) => {
-    const pageCount = postCount ? Math.ceil(postCount / 10) : 1;
-    return (
-        <section>
-            <h1 className={cx('post-count normal')}>총 {postCount}개의 글이 있습니다</h1>
-            <table className={cx('posts normal')}>
-                <colgroup>
-                    <col style={{ width: '54px' }} />
-                    <col style={{ width: '175px' }} />
-                    <col />
-                    <col style={{ width: '150px' }} />
-                </colgroup>
-                <thead>
-                    <tr>
-                        <th scope="col">글 번호</th>
-                        <th scope="col">글쓴이</th>
-                        <th scope="col">제목</th>
-                        <th scope="col">작성일</th>
-                    </tr>
-                </thead>
-                {children}
-            </table>
-            {
-                createArray(pageCount).map(e => {
-                    return <button key={e} onClick={() => { onPageMove(e + 1) }}>{e + 1}</button>
-                })
-            }
-        </section>
-    );
+  const pageCount = postCount ? Math.ceil(postCount / 10) : 1;
+  return (
+    <section>
+      <h1 className={cx('post-count normal')}>총 {postCount}개의 글이 있습니다</h1>
+      <table className={cx('posts normal')}>
+        <colgroup>
+          <col style={{ width: '54px' }} />
+          <col style={{ width: '175px' }} />
+          <col />
+          <col style={{ width: '150px' }} />
+        </colgroup>
+        <thead>
+          <tr>
+            <th scope="col">글 번호</th>
+            <th scope="col">글쓴이</th>
+            <th scope="col">제목</th>
+            <th scope="col">작성일</th>
+          </tr>
+        </thead>
+        {children}
+      </table>
+      {
+        createArray(pageCount).map(e => {
+          return (
+            <button
+              key={e}
+              onClick={() => {
+                onPageMove(e + 1);
+                window.scrollTo(0, 300);
+              }}
+            >
+              {e + 1}
+            </button>
+          )
+        })
+      }
+    </section>
+  );
 };
 
 export default PostSection;
