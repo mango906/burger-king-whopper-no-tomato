4007f31f732871266d58e23b96fd8ddbdf2417ba
seoji <idw5780@dgsw.hs.kr>
2019-05-20 19:37:38 +0900

    [ADD] Delete Comment API
    페이지 id와 댓글 id를 받아 댓글을 삭제하는 api

--- backend/src/api/comment/comment.ctrl.js
+++ backend/src/api/comment/comment.ctrl.js
@@ -1,5 +1,4 @@
 const post = require('database/models/post');
-const Comment = post.Comment;
 
 exports.createComment = async (req, res) => {
     const {
@@ -16,6 +15,7 @@ exports.createComment = async (req, res) => {
             res.status(404).json({
                 message: '글이 없어요',
             });
+            return;
         }
         const { comments } = find;
         let idx = comments.length ? comments[0].id + 1 : 1;
@@ -32,6 +32,47 @@ exports.createComment = async (req, res) => {
         res.status(200).json({
             message: '오홍홍 좋아용',
             comment: newComment,
+        });
+    } catch (error) {
+        const result = {
+            message: '서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요',
+        };
+        console.log(error.message);
+        res.status(500).json(result);
+    }
+}
+
+exports.deleteComment = async (req, res) => {
+    const {
+        postId,
+        commentId,
+        password
+    } = req.params;
+    try{
+        const find = await post.findOne({ id: postId });
+        if(!find){
+            res.status(404).json({
+                message: '글이 없어요',
+            });
+            return;
+        }
+        const findComment = find.comments.find(comment => comment.id === parseInt(commentId));
+        if(!findComment){
+            res.status(404).json({
+                message: '댓글이 없어요',
+            });
+            return;
+        }
+        if(post.cryptoPassword(password) !== findComment.password){
+            res.status(401).json({
+                message: '비밀 번호가 틀렸어요',
+            })
+            return;
+        }
+        find.comments = find.comments.filter(comment => comment.id !== parseInt(commentId));
+        find.save();
+        res.status(200).json({
+            message: '오홍홍 좋아용'
         });
     } catch (error) {
         const result = {
--- backend/src/api/comment/index.js
+++ backend/src/api/comment/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const commentCtrl = require('./comment.ctrl');
 
 router.route('/:id').post(commentCtrl.createComment);
+router.route('/:postId/:commentId/:password').delete(commentCtrl.deleteComment);
 
 module.exports = router;
