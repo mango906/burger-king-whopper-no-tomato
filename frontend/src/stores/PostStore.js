68b03d83e175f59fb2b85fbfea8dd7c7b86b5401
seoji <idw5780@dgsw.hs.kr>
2019-05-21 17:57:51 +0900

    [FIX] PostStore
    내용을 받아 리스트에 있는 글 내용을 변경하는 updatePostTitle  action 작성

--- frontend/src/stores/PostStore.js
+++ frontend/src/stores/PostStore.js
@@ -1,6 +1,6 @@
-import { observable, action, toJS } from 'mobx';
-import axios from 'axios';
-import { SERVER } from 'config/config.json';
+import { observable, action } from "mobx";
+import axios from "axios";
+import { SERVER } from "config/config.json";
 
 class PostStore {
   @observable postList = [];
@@ -14,7 +14,8 @@ class PostStore {
   @action
   getPosts = async () => {
     this.loading = true;
-    await axios.get(`${SERVER}/post/${this.page}`)
+    await axios
+      .get(`${SERVER}/post/${this.page}`)
       .then(res => {
         this.postList = res.data.posts;
         this.loading = false;
@@ -22,67 +23,70 @@ class PostStore {
       })
       .catch(e => {
         console.log(e);
-      })
-  }
+      });
+  };
   @action
   getCount = async () => {
-    await axios.get(`${SERVER}/post/count`)
+    await axios
+      .get(`${SERVER}/post/count`)
       .then(res => {
         this.postCount = res.data.count;
       })
       .catch(e => {
         console.log(e);
-      })
-  }
+      });
+  };
   @action
-  setViewContent = async (data) => {
-   const { comments, ...post } = data;
-   this.viewComments = comments;
-   this.viewPost = post;
-  }
+  setViewContent = async data => {
+    const { comments, ...post } = data;
+    this.viewComments = comments;
+    this.viewPost = post;
+  };
   @action
   addPost = ({ post }) => {
     const { _id, __v, password, ...data } = post;
     this.postList.unshift(data);
     this.postCount++;
     this.postList.length > 10 && this.postList.pop();
-  }
+  };
   @action
-  removePost = async (password) => {
+  removePost = async password => {
     const { id } = this.viewPost;
-    try{
+    try {
       await axios.delete(`${SERVER}/post/${id}/${password}`);
       this.removePostToList(id);
       return true;
-    }catch(e){
+    } catch (e) {
       console.log(e);
       return false;
     }
-  }
+  };
   @action
   removeComment = async (password, commentId) => {
     const { id } = this.viewPost;
     try {
-      await axios.delete(`${SERVER}/comment/${id}/${commentId}/${password}`)
-      this.viewComments = this.viewComments.filter(comment => comment.id !== commentId);
+      await axios.delete(`${SERVER}/comment/${id}/${commentId}/${password}`);
+      this.viewComments = this.viewComments.filter(
+        comment => comment.id !== commentId
+      );
       if (this.apiCall) {
         const find = this.postList.find(post => post.id === id);
         find.comments.pop();
       }
       return true;
-    } catch(e) {
+    } catch (e) {
       return false;
-    };
-  }
+    }
+  };
   @action
-  removePostToList = async (id) => {
-    if(this.apiCall){
+  removePostToList = async id => {
+    if (this.apiCall) {
       const find = this.postList.find(post => post.id === parseInt(id));
       this.postList.remove(find);
       this.postCount--;
       await this.getPosts();
     }
-  }
+  };
   @action
   updateViewComments = (data, postId) => {
     if (this.apiCall) {
@@ -90,15 +94,22 @@ class PostStore {
       find.comments.push(data);
     }
     this.viewComments = [...this.viewComments, data];
-  }
+  };
   @action
-  movePage = async (page) => {
+  movePage = async page => {
     if (this.page === page) {
       return;
     }
     this.page = page;
     await this.getPosts();
-  }
+  };
+  @action
+  updatePostTitle = async post => {
+    if (this.apiCall) {
+      const find = this.postList.find(e => e.id === post.id);
+      find.title = post.title;
+    }
+  };
 }
 
 export default PostStore;
