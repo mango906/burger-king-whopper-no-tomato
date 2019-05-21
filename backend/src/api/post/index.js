45adadb96551ee175766125ab449c54f000ebd4d
seoji <idw5780@dgsw.hs.kr>
2019-05-21 18:01:51 +0900

    [ADD] Update post API
    글을 수정하는 api와 게시글의 비밀번호를 체크하는 api 작성

--- backend/src/api/post/index.js
+++ backend/src/api/post/index.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
-const postCtrl = require('./post.ctrl');
+const router = require("express").Router();
+const postCtrl = require("./post.ctrl");
 
-router.route('/').post(postCtrl.createPost);
-router.route('/:id/:password').delete(postCtrl.deletePost);
-router.route('/count').get(postCtrl.count);
-router.route('/:page').get(postCtrl.getPosts);
-router.route('/view/:id').get(postCtrl.viewPost);
+router.route("/").post(postCtrl.createPost);
+router.route("/:id").patch(postCtrl.updatePost);
+router.route("/:id/:password").delete(postCtrl.deletePost);
+router.route("/count").get(postCtrl.count);
+router.route("/:page").get(postCtrl.getPosts);
+router.route("/view/:id").get(postCtrl.viewPost);
+router.route("/check/:id/:password").get(postCtrl.checkPassword);
 
 module.exports = router;
--- backend/src/api/post/post.ctrl.js
+++ backend/src/api/post/post.ctrl.js
@@ -1,133 +1,198 @@
-const post = require('database/models/post');
+const post = require("database/models/post");
 
 exports.count = async (req, res) => {
-    try {
-        const postCount = await post.count();
-        res.status(200).json({ count: postCount });
-    } catch (error) {
-        const result = {
-            message: '서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요',
-        };
-        console.log(error.message);
-        res.status(500).json(result);
-    }
-}
+  try {
+    const postCount = await post.count();
+    res.status(200).json({ count: postCount });
+  } catch (error) {
+    const result = {
+      message: "서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요"
+    };
+    console.log(error.message);
+    res.status(500).json(result);
+  }
+};
 
 exports.getPosts = async (req, res) => {
-    const {
-        page,
-    } = req.params;
-    try {
-        const posts = await post.find({},
-            { '__v': false, '_id': false, 'content': false, 'password': false, 'comments.password': false })
-            .sort({ id: -1 })
-            .limit(10)
-            .skip(parseInt(page - 1, 10) * 10);
-        res.status(200).json({
-            message: '오홍홍 좋아용',
-            posts,
-        });
-    } catch (error) {
-        const result = {
-            message: '서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요',
-        };
-        console.log(error.message);
-        res.status(500).json(result);
-    }
+  const { page } = req.params;
+  try {
+    const posts = await post
+      .find(
+        {},
+        {
+          __v: false,
+          _id: false,
+          content: false,
+          password: false,
+          "comments.password": false
+        }
+      )
+      .sort({ id: -1 })
+      .limit(10)
+      .skip(parseInt(page - 1, 10) * 10);
+    res.status(200).json({
+      message: "오홍홍 좋아용",
+      posts
+    });
+  } catch (error) {
+    const result = {
+      message: "서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요"
+    };
+    console.log(error.message);
+    res.status(500).json(result);
+  }
 };
 
 exports.viewPost = async (req, res) => {
-    const {
-        id
-    } = req.params;
-    try {
-        const view = await post.findOne({ id }, { '__v': false, '_id': false, 'password': false, 'comments.password': false });
-        if (view) {
-            view.comments.sort(function (a, b) {
-                return a.created - b.created;
-            })
-            res.status(200).json({
-                message: '오홍홍 조와용',
-                view
-            })
-        } else {
-            res.status(404).json({
-                message: '없어용',
-            })
-        }
-    } catch (error) {
-        const result = {
-            message: '서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요',
-        };
-        console.log(error.message);
-        res.status(500).json(result);
+  const { id } = req.params;
+  try {
+    const view = await post.findOne(
+      { id },
+      { __v: false, _id: false, password: false, "comments.password": false }
+    );
+    if (view) {
+      view.comments.sort(function(a, b) {
+        return a.created - b.created;
+      });
+      res.status(200).json({
+        message: "오홍홍 조와용",
+        view
+      });
+    } else {
+      res.status(404).json({
+        message: "없어용"
+      });
     }
+  } catch (error) {
+    const result = {
+      message: "서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요"
+    };
+    console.log(error.message);
+    res.status(500).json(result);
+  }
 };
 
 exports.createPost = async (req, res) => {
-    const {
-        title,
-        content,
-        author,
-        password,
-    } = req.body;
+  const { title, content, author, password } = req.body;
 
-    try {
-        let id;
-        const lastPost = await post
-            .findOne()
-            .sort({ created: -1 })
-            .limit(1);
-        if (lastPost === null) {
-            id = 1;
-        } else {
-            console.log(lastPost);
-            id = lastPost.id + 1;
-        }
-        const newPost = await post.create(id, author, title, content, password);
-        res.status(200).json({
-            message: '오홍홍 좋아용',
-            post: newPost,
-        })
-    } catch (error) {
-        const result = {
-            message: '서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요',
-        };
-        console.log(error.message);
-        res.status(500).json(result);
+  try {
+    let id;
+    const lastPost = await post
+      .findOne()
+      .sort({ created: -1 })
+      .limit(1);
+    if (lastPost === null) {
+      id = 1;
+    } else {
+      console.log(lastPost);
+      id = lastPost.id + 1;
     }
+    const newPost = await post.create(id, author, title, content, password);
+    res.status(200).json({
+      message: "오홍홍 좋아용",
+      post: newPost
+    });
+  } catch (error) {
+    const result = {
+      message: "서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요"
+    };
+    console.log(error.message);
+    res.status(500).json(result);
+  }
 };
 
 exports.deletePost = async (req, res) => {
-    const {
-        id,
-        password
-    } = req.params;
-    try {
-        const find = await post.findOne({id});
-        if(!find){
-            const result = {
-                message: '해당 idx의 대기 글이 없어요',
-            };
-            res.status(404).json(result);
-            return;
-        }
-        if(!find.checkPassword(password)){
-            const result = {
-                message: '패스워드가 틀립니다',
-            };
-            res.status(401).json(result);
-            return;
-        }
-        await post.deleteOne({id});
-        res.status(200).json({
-            message: '오홍홍 좋아용'
-        })
-    } catch(error) {
-        const result = {
-            message: '서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요',
-        };
-        console.log(error.message);
-        res.status(500).json(result);
+  const { id, password } = req.params;
+  try {
+    const find = await post.findOne({ id });
+    if (!find) {
+      const result = {
+        message: "해당 id의 글이 없습니다"
+      };
+      res.status(404).json(result);
+      return;
     }
-}
+    if (!find.checkPassword(password)) {
+      const result = {
+        message: "패스워드가 틀립니다"
+      };
+      res.status(401).json(result);
+      return;
+    }
+    await post.deleteOne({ id });
+    res.status(200).json({
+      message: "오홍홍 좋아용"
+    });
+  } catch (error) {
+    const result = {
+      message: "서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요"
+    };
+    console.log(error.message);
+    res.status(500).json(result);
+  }
+};
+
+exports.checkPassword = async (req, res) => {
+  const { id, password } = req.params;
+  try {
+    const find = await post.findOne({ id });
+    if (!find) {
+      const result = {
+        message: "해당 id의 글이 없습니다"
+      };
+      res.status(404).json(result);
+      return;
+    }
+    if (!find.checkPassword(password)) {
+      const result = {
+        message: "패스워드가 틀립니다"
+      };
+      res.status(401).json(result);
+      return;
+    }
+    res.status(200).json({
+      message: "오홍홍 좋아용"
+    });
+  } catch (error) {
+    const result = {
+      message: "서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요"
+    };
+    console.log(error.message);
+    res.status(500).json(result);
+  }
+};
+
+exports.updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { password, title, content } = req.body;
+  try {
+    const find = await post.findOne({ id });
+    if (!find) {
+      const result = {
+        message: "해당 id의 글이 없습니다"
+      };
+      res.status(404).json(result);
+      return;
+    }
+    if (!find.checkPassword(password)) {
+      const result = {
+        message: "패스워드가 틀립니다"
+      };
+      res.status(401).json(result);
+      return;
+    }
+    find.title = title;
+    find.content = content;
+    find.save();
+    res.status(200).json({
+      message: "오홍홍 좋아용",
+      post: find
+    });
+  } catch (error) {
+    const result = {
+      message: "서버 에러네요 괜찮아요 원숭이들이 금방 고칠거에요"
+    };
+    console.log(error.message);
+    res.status(500).json(result);
+  }
+};
