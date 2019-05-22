aaa77fb0baf6b4b7cbddc7ddcfca67125f6379bc
seoji <idw5780@dgsw.hs.kr>
2019-05-22 15:31:34 +0900

    [FIX] PageTemplate
    줄바꿈, 들여쓰기 정리

--- frontend/src/components/common/PageTemplate/PageTemplate.js
+++ frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import styles from './PageTemplate.scss';
-import classNames from 'classnames/bind';
-import { Link, withRouter } from 'react-router-dom';
+import React from "react";
+import styles from "./PageTemplate.scss";
+import classNames from "classnames/bind";
+import { Link, withRouter } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 const PageTemplate = ({ children, location }) => {
-    return (
-        <div className={cx('post-template')}>
-            <header>
-                <h1 className={cx('header-title light')}>
-                    <Link to="/">
-                        버거킹 와퍼에
-                    <br />
-                        토마토 빼고 주세요
-                    </Link>
-                </h1>
-            </header>
-            {children}
-            <footer>
-                <div className="buttons">
-                    {
-                        location.pathname !== "/" &&
-                        <Link to="/" className={cx('btn list')}>목록으로</Link>
-                    }
-                    {
-                        location.pathname !== "/write" &&
-                        <Link to="/write" className={cx('btn new-post')}>글 쓰기</Link>
-                    }
-                </div>
-            </footer>
-        </div >
-    );
+  return (
+    <div className={cx("post-template")}>
+      <header>
+        <h1 className={cx("header-title light")}>
+          <Link to="/">
+            버거킹 와퍼에
+            <br />
+            토마토 빼고 주세요
+          </Link>
+        </h1>
+      </header>
+      {children}
+      <footer>
+        <div className="buttons">
+          {location.pathname !== "/" && (
+            <Link to="/" className={cx("btn list")}>
+              목록으로
+            </Link>
+          )}
+          {location.pathname !== "/write" && (
+            <Link to="/write" className={cx("btn new-post")}>
+              글 쓰기
+            </Link>
+          )}
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default withRouter(PageTemplate);
