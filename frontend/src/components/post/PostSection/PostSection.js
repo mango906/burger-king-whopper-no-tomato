dee49bc7cdfe6c173bf7d3c8811f4864ce45a401
seoji <idw5780@dgsw.hs.kr>
2019-05-22 20:40:45 +0900

    [FIX] PostSection
    글이 없을때 글이 없다고 보여주게 수정했습니다

--- frontend/src/components/post/PostSection/PostSection.js
+++ frontend/src/components/post/PostSection/PostSection.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from "react";
 import Pagination from "react-js-pagination";
-import styles from './PostSection.scss';
-import classNames from 'classnames/bind';
+import styles from "./PostSection.scss";
+import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
 
 const PostSection = ({ children, postCount, onPageMove, page }) => {
   return (
     <section>
-      <h1 className={cx('post-count normal')}>총 {postCount}개의 글이 있습니다</h1>
-      <table className={cx('posts normal')}>
+      <h1 className={cx("post-count normal")}>
+        총 {postCount}개의 글이 있습니다
+      </h1>
+      <table className={cx("posts normal")}>
         <colgroup>
-          <col style={{ width: '54px' }} />
-          <col style={{ width: '175px' }} />
+          <col style={{ width: "54px" }} />
+          <col style={{ width: "175px" }} />
           <col />
-          <col style={{ width: '150px' }} />
+          <col style={{ width: "150px" }} />
         </colgroup>
         <thead>
           <tr>
@@ -26,20 +28,26 @@ const PostSection = ({ children, postCount, onPageMove, page }) => {
         </thead>
         {children}
       </table>
-      <Pagination
+      {postCount === 0 ? (
+        <div className={cx("no-post")}>
+          <h1>아직 글이 없어요!</h1>
+        </div>
+      ) : (
+        <Pagination
           activePage={page}
           itemsCountPerPage={10}
           totalItemsCount={postCount}
           pageRangeDisplayed={5}
-          onChange={(page) => {
+          onChange={page => {
             onPageMove(page);
-            window.scrollTo(0,300);
+            window.scrollTo(0, 300);
           }}
           itemClass="page-btn"
           activeClass="select"
           innerClass="Pagination"
           disabledClass="disable"
-      />
+        />
+      )}
     </section>
   );
 };
